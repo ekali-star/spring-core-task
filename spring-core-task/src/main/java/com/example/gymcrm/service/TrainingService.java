62918643a0be2a6d89d3fc113df822626422fafe
ekali-star <ekali23@freeuni.edu.ge>
2026-02-17 21:33:31 +0400

    added service classes, credentials generator and getter/setters for modules to use in service classes

---
+++ spring-core-task/src/main/java/com/example/gymcrm/service/TrainingService.java
@@ -0,0 +1,46 @@
+package com.example.gymcrm.service;
+
+import com.example.gymcrm.dao.TrainingDao;
+import com.example.gymcrm.model.Training;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class TrainingService {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(TrainingService.class);
+
+    private TrainingDao trainingDao;
+
+    private final AtomicLong idGenerator = new AtomicLong(1);
+
+    @Autowired
+    public void setTrainingDao(TrainingDao trainingDao) {
+        this.trainingDao = trainingDao;
+    }
+
+    public Training create(Training training) {
+
+        Long id = idGenerator.getAndIncrement();
+
+        trainingDao.save(id, training);
+
+        log.info("Training created: id={}", id);
+
+        return training;
+    }
+
+    public Training findById(Long id) {
+        return trainingDao.findById(id);
+    }
+
+    public Collection<Training> findAll() {
+        return trainingDao.findAll();
+    }
+}
