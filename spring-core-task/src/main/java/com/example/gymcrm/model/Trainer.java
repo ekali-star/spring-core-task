8e53ff227ddd26e674130d305b36c640c962a4ef
ekali-star <ekali23@freeuni.edu.ge>
2026-02-17 16:03:47 +0400

    added model entities (User, Trainee, Trainer, Training)

---
+++ spring-core-task/src/main/java/com/example/gymcrm/model/Trainer.java
@@ -0,0 +1,6 @@
+package com.example.gymcrm.model;
+
+public class Trainer extends User{
+    private String specialization;
+    private Long userID;
+}
---
+++ spring-core-task/src/main/java/com/example/gymcrm/model/Training.java
@@ -0,0 +1,13 @@
+package com.example.gymcrm.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Training {
+    private Long traineeID;
+    private Long trainerID;
+    private String trainingName;
+    private String trainingType;
+    private LocalDate trainingDate;
+    private int trainingDuration;
+}
